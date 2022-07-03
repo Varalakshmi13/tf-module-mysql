3c39a83dc8c26638cbf4fa410c613111abd377da
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-07-03 20:53:24 +0530

    Updating tf-module-mysql

---
+++ cluster.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "allow_mysql" {
+  name        = "roboshop-mysql-${var.ENV}"
+  description = "roboshop-mysql-${var.ENV}"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-redis-${var.ENV}"
+  }
+}
+
+resource "aws_db_instance" "mysql
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
